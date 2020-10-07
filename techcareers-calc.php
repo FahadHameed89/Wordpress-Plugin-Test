d965073796de8c8bf12c18db06409006eea6af74
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-07 14:36:54 -0600

    Added initial description file

---
+++ techcareers-calc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       TECHCareers Calculator Plugin
+ * Plugin URI:        https://github.com/FahadHameed89/Wordpress-Plugin-Test
+ * Description:       A simple PHP Calculator
+ * Version:           1.0
+ * Requires at least: 5
+ * Requires PHP:      7.4
+ * Author:            Fahad Hameed
+ * Author URI:        https://github.com/FahadHameed89
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
